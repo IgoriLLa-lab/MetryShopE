bbbbf6a93e5c94969b68589109a840a2b284436e
Igor Kolesnik <crocusik@mail.ru>
2022-02-28 18:25:07 +0500

    Initial commit

---
+++ src/main/java/com/metryfabricshopnew/MetryfabricshopnewApplication.java
@@ -0,0 +1,13 @@
+package com.metryfabricshopnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetryfabricshopnewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetryfabricshopnewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/metryfabricshopnew/MetryfabricshopnewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metryfabricshopnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetryfabricshopnewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
